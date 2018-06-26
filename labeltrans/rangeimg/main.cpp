9070db6130649dafd3965230b30d53136acf372e
husong998 <34832485+husong998@users.noreply.github.com>
2018-06-26 10:17:39 +0800

    Update main.cpp

--- labeltrans/rangeimg/main.cpp
+++ labeltrans/rangeimg/main.cpp
@@ -6,11 +6,14 @@ using namespace cv;
 
 int main()
 {
+  //Change the content of this string below to the path of KITTI Velodyne data on your disk
   string file="/home/husong/kitti/2011_09_26/2011_09_26_drive_0001_sync/velodyne_points/data/0000000000.bin";
+
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = readkitti(file);
-  //for(int i=0; i<cloud->points.size(); i++)
-    //printf("%.2f %.2f %.2f\n",cloud->points[i].x,cloud->points[i].y,cloud->points[i].z);
+  
   Mat rangeimg = GetRangeIMG(cloud);
+  
   imwrite("test.png",rangeimg);
+  
   return 0;
 }
